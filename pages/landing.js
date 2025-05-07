15f9d758ac449a51d58a2d92438df824554a3afb
Gramz96 <mxbillygrahamlema@students.everettcc.edu>
2025-05-07 06:31:42 +0000

    Landing page changes

--- pages/landing.js
+++ pages/landing.js
@@ -19,7 +19,6 @@ import styles from "/styles/jss/nextjs-material-kit/pages/landingPage.js";
 
 // Sections for this page
 import ProductSection from "/pages-sections/LandingPage-Sections/ProductSection.js";
-import TeamSection from "/pages-sections/LandingPage-Sections/TeamSection.js";
 import WorkSection from "/pages-sections/LandingPage-Sections/WorkSection.js";
 
 const dashboardRoutes = [];
@@ -43,22 +42,19 @@ export default function LandingPage(props) {
         }}
         {...rest}
       />
-      <Parallax filter responsive image="/img/landing-bg.jpg">
+      <Parallax filter responsive image="/img/bg4.jpg">
         <div className={classes.container}>
           <GridContainer>
             <GridItem xs={12} sm={12} md={6}>
-              <h1 className={classes.title}>Your Story Starts With Us.</h1>
+              <h1 className={classes.title}>Music Gen</h1>
               <h4>
-                Every landing page needs a small description after the big bold
-                title, that{"'"}s why we added this text here. Add here all the
-                information that can make you or your product create the first
-                impression.
+                This is an AI Music Genrator that can create a one minute song depending on your selection..
               </h4>
               <br />
               <Button
                 color="danger"
                 size="lg"
-                href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ref=creativetim"
+                href="#"
                 target="_blank"
                 rel="noopener noreferrer"
               >
